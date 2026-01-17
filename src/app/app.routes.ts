660929752e2a030a2a784a0a2d32841cf4e123fc
Gabriel César Rogério <gabriel.cesar.rogerio@outlook.com>
2026-01-16 21:50:47 -0300

    adding base layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout';
 import { DashboardComponent } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardComponent
+      }
+    ]
   }
 ];
---
+++ src/app/core/layout/layout.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule
+  ],
+  templateUrl: './layout.html',
+  styleUrls: ['./layout.scss']
+})
+export class LayoutComponent {}
